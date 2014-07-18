6eea931e7c29d7bd97ec353dd0df7a2953b883e9
bekeomut <bekeomut@github.com>
2014-07-18 16:15:31 +0700

    add new 2 file

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-Fabric-Example
-==============
+
+Cài đặt wordpress từ xa bằng fabric trên Ubuntu:
+================================================
+
+
+Trước khi sử dụng :
+------------------
+
+**Giới thiệu qua về Fabric và Wordpress**<br>
+  -Fabric là một thư viện và các câu lệnh được viết bằng python để giúp nhưng người quản trị có thể nhanh chóng thực hiện các task vụ thông qua ssh remote.<br>
+  -ta có thể cài đặt fabric bằng lênh (ubuntu):<br>
+  `sudo apt-get install fabric`<br>
+  <br>
+  -Wordpress là một framework của php gíup người dùng có thể tạp các blog đẹp và nhanh chóng .
+  
+**Giới thiệu bài Lab**<br>
+  trong lab này sẽ hướng dẫn mọi người sử dụng Fabric để cài đặt từ xa wordpress framework.<br><br>
+**Yêu Cầu các gói**<br>
+  -Trên máy remote : `ssh python2.6+ git`<br>
+  -trên máy host : `ssh`<br><br>
+  
+Bắt đầu Cài đặt
+---------------
+Trên Terminal lần lượt chạy các lệnh: 
+  >  git clone https://github.com/bekeomut/Fabric-Wordpress<br>
+  cd Fabric-Wordpress<br>
+  fab deploy
+  
+  
+sau khi chạy lệnh . nhập một số thông tin cần thiết trên terminal => done !
---
+++ fabfile.py
@@ -0,0 +1,81 @@
+__author__ = 'cherry'
+
+from fabric.api import *
+from fabric.operations import sudo
+
+""" Provide all user and pass to automation ! """
+
+env.hosts = raw_input('Enter Host : ')
+env.user = raw_input('Enter User : ')
+mysql_pwd = raw_input('provide mysql database root password : ')
+wp_db_name = raw_input('provide wordpress database name : ')
+wp_user_name = raw_input('provide wordpress database user name : ')
+wp_user_pwd = raw_input('provide wordpress database user password : ')
+
+
+def deploy():
+    updatesystem()
+    install_server()
+    config_mysql()
+    install_wp()
+    init_wp()
+    showos()
+
+
+def updatesystem():
+    sudo('apt-get -y update')
+
+
+def mysqlcommand():
+        sudo("mysql -p%s -e 'DROP DATABASE IF EXISTS %s;'" % (mysql_pwd, wp_db_name))
+        sudo("mysql -p%s -e 'CREATE DATABASE %s;'" % (mysql_pwd, wp_db_name,))
+        sudo('mysql -p%s -e "CREATE USER %s@localhost IDENTIFIED BY \'%s\';"' % (mysql_pwd, wp_user_name, wp_user_pwd))
+        sudo("mysql -p%s -e 'GRANT ALL PRIVILEGES ON %s .* TO %s@localhost;'" % (mysql_pwd, wp_db_name, wp_user_name))
+        sudo("mysql -p%s -e 'FLUSH PRIVILEGES;'" % mysql_pwd)
+
+
+def install_server():
+    sudo(
+        'apt-get install -y php5-gd libssh2-php php5-mysql php5-common php5-cli libapache2-mod-php5 php5-mcrypt apache2'
+    )
+    checkapache = run('service httpd status')
+    if 'running' in checkapache:
+        pass
+    else:
+        sudo('service apache2 start')
+    run('sleep 3')
+
+
+def config_mysql():
+    sudo('echo mysql-server mysql-server/root_password password %s | debconf-set-selections' % mysql_pwd)
+    sudo('echo mysql-server mysql-server/root_password_again password %s | debconf-set-selections' % mysql_pwd)
+    sudo('apt-get -y install mysql-server python-mysqldb curl expect')
+    checkmysql = run('service mysql status')
+    if 'running' in checkmysql:
+        mysqlcommand()
+    else:
+        sudo('service mysql start')
+        mysqlcommand()
+
+
+def install_wp():
+    with cd("~/"):
+        run('wget http://wordpress.org/latest.tar.gz')
+        run("sleep 5")
+        run('tar zxvf latest.tar.gz')
+    with cd('~/wordpress'):
+        run('mv wp-config-sample.php wp-config.php')
+        run("sed -i 's/database_name_here/%s/g' wp-config.php" % wp_db_name)
+        run("sed -i 's/username_here/%s/g' wp-config.php" % wp_user_name)
+        run("sed -i 's/password_here/%s/g' wp-config.php" % wp_user_pwd)
+
+
+def init_wp():
+    sudo('rsync -avP ~/wordpress/ /var/www/html/')
+    sudo('mkdir /var/www/html/wp-content/uploads')
+    sudo("rm /var/www/html/index.html")
+    sudo('service apache2 restart')
+
+
+def showos():
+    print "go to http://%s/ to test your wordpress website !" % env.hosts
